a41212a5da6f8109f08ba11333c65fe261349599
jungwon <wjddnjs@snu.ac.kr>
2025-04-11 23:27:22 +0900

    TDD lab 완료 - calculator예외 처리 및 테스트 통과

--- calculator.cpp
+++ calculator.cpp
@@ -1,18 +1,39 @@
-#include "calculator.h"
-#include <stdexcept>
+#include "Calculator.h"
+#include <stdexcept>    // 예외 사용
+#include <limits>       // INT_MAX, INT_MIN
 
 int Calculator::add(int a, int b) {
-    // TODO
+    if (b > 0 && a > std::numeric_limits<int>::max() - b)
+        throw std::overflow_error("Addition overflow");
+    if (b < 0 && a < std::numeric_limits<int>::min() - b)
+        throw std::overflow_error("Addition underflow");
+    return a + b;
 }
 
 int Calculator::sub(int a, int b) {
-    // TODO
+    if (b < 0 && a > std::numeric_limits<int>::max() + b)
+        throw std::overflow_error("Subtraction overflow");
+    if (b > 0 && a < std::numeric_limits<int>::min() + b)
+        throw std::overflow_error("Subtraction underflow");
+    return a - b;
 }
 
 int Calculator::mul(int a, int b) {
-    // TODO
+    if (a > 0 && b > 0 && a > std::numeric_limits<int>::max() / b)
+        throw std::overflow_error("Multiplication overflow");
+    if (a > 0 && b < 0 && b < std::numeric_limits<int>::min() / a)
+        throw std::overflow_error("Multiplication underflow");
+    if (a < 0 && b > 0 && a < std::numeric_limits<int>::min() / b)
+        throw std::overflow_error("Multiplication underflow");
+    if (a < 0 && b < 0 && a < std::numeric_limits<int>::max() / b)
+        throw std::overflow_error("Multiplication overflow");
+    return a * b;
 }
 
 int Calculator::div(int a, int b) {
-    // TODO
+    if (b == 0)
+        throw std::invalid_argument("Division by zero");
+    if (a == std::numeric_limits<int>::min() && b == -1)
+        throw std::overflow_error("Division overflow");
+    return a / b;
 }
